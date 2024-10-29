9215c4e3ae8ec19aa420b4acd3aa17186078f5ff
Bryan Patricio Juarez <jqk.juarez@gmail.com>
2024-10-28 23:56:45 -0500

    first send

--- main.py
+++ main.py
@@ -2,14 +2,126 @@ import streamlit as st
 from keras.models import load_model
 from PIL import Image
 import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
+import PIL
+import pathlib
+
+import os
+from PIL import Image
+import tensorflow as tf
+import keras
+import keras.callbacks
+from keras.callbacks import TensorBoard
+import os, warnings
+import matplotlib.pyplot as plt
+from matplotlib import gridspec
+import matplotlib.pyplot as plt
+import seaborn as sns
+from mlxtend.plotting import plot_confusion_matrix
+from sklearn.metrics import confusion_matrix
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import keras
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras import callbacks
+from keras.models import Sequential
+from tensorflow import keras
+from keras.layers import Dense, Conv1D, MaxPooling1D, Dropout, BatchNormalization, SimpleRNN, Flatten, LSTM
+import imblearn
+from imblearn.over_sampling import SMOTE
+from collections import Counter
+from numpy import where
+from matplotlib import pyplot
+import matplotlib.pyplot as plt
+
+import torch,time,os, shutil
+#import models
+import utils
+import pandas as pd
+import numpy as np
+import torch, time, os, shutil
+#import models, utils
+import numpy as np
+import pandas as pd
+import torch
+#from tensorboard_logger import Logger
+from torch import nn, optim
+from torch.utils.data import DataLoader
+import tensorflow as tf
+from torch import nn, optim
+from torch.utils.data import DataLoader
+from tensorflow.keras.callbacks import EarlyStopping
+import matplotlib.pyplot as plt
+
+plt.rc('figure', autolayout=True)
+plt.rc('axes', labelweight='bold', labelsize='large',
+       titleweight='bold', titlesize=18, titlepad=10)
+plt.rc('animation', html='html5')
+
+# Setup feedback system
+#from learntools.core import binder
+#binder.bind(globals())
+#from learntools.deep_learning_intro.ex3 import *
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import make_column_transformer, make_column_selector
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split
+# Setup feedback system
+
+
+
+#from learntools.core import binder
+#binder.bind(globals())
+#from learntools.computer_vision.ex5 import *
+
+# Imports
+import keras
+import keras.callbacks
+from keras.callbacks import TensorBoard
+import os, warnings
+import matplotlib.pyplot as plt
+from matplotlib import gridspec
+
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image_dataset_from_directory
+import collections.abc
+import functools
+from typing import Any, Callable, Iterable, Iterator, Union
+
+import numpy as np
+
+import tensorflow as tf
+from tensorflow_datasets.core import tf_compat
+from tensorflow_datasets.core import utils
+from tensorflow_datasets.core.utils import type_utils
+import tensorflow_datasets as tfds
+import numpy as np
+import matplotlib.pyplot as plt
+import keras
+from keras.layers import *
+from keras.models import *
+from keras.preprocessing import image
+from keras.callbacks import EarlyStopping
+from keras.callbacks import ModelCheckpoint
+from keras.callbacks import LearningRateScheduler
+from sklearn.metrics import classification_report
 
 from util import classify, set_background
+
 
 
 set_background('./bgs/bg5.png')
 
 # set title
-st.title('Pneumonia classification')
+st.title('VGG19 Chest X ray  Diagnosis Tool')
 
 # set header
 st.header('Please upload a chest X-ray image')
@@ -18,7 +130,12 @@ st.header('Please upload a chest X-ray image')
 file = st.file_uploader('', type=['jpeg', 'jpg', 'png'])
 
 # load classifier
-model = load_model('./model/pneumonia_classifier.h5')
+
+model_path = r'C:\Users\bjxtr\Downloads\vgg19fulllayersmore_trained.keras'
+
+# Load the model
+model = tf.keras.models.load_model(model_path)
+#model = load_model('./model/pneumonia_classifier.h5')
 
 # load class names
 with open('./model/labels.txt', 'r') as f:
---
+++ readme.txt
@@ -0,0 +1,8 @@
+Activate virtual enviroment
+    .venv\Scripts\Activate
+install packages using
+    pip install -r requirements.txt
+Run app
+    streamlit run main.py                      
+                   
+   
